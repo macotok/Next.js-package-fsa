0aa34f03707f3aef19502609f7c7a076809d1f85
macotok <monochromer@r8.ucom.ne.jp>
2020-06-26 00:33:51 +0900

    feat: implement api

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosPromise } from 'axios';
+import { MicroCmsQueryType, GetNewsResultType } from '../actions/getNews/';
+
+const instance = axios.create({
+  baseURL: process.env.API_MICRO_CMS_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-API-KEY': process.env.MICRO_CMS_X_API_KEY,
+  },
+  timeout: 1000,
+});
+
+export const getNews = (
+  params: MicroCmsQueryType
+): AxiosPromise<GetNewsResultType> =>
+  instance({
+    params,
+    method: 'GET',
+    url: 'news',
+  });
